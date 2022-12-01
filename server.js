7d9d1f884a937783c9682cb73c0b71132f342efe
ctsmit <114516481+ctsmit@users.noreply.github.com>
2022-12-01 15:56:07 -0500

    SHow view shows req.params.id

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ let pokemon = require("./models/pokemon")
 app.set("view engine", "jsx")
 app.engine("jsx", require("express-react-views").createEngine())//!creates engine?
 
+//!Index
 app.get('/', (req,res) => {
    res.send("Welcome to the Pokemon App!")
 })
 app.get('/pokemon', (req, res) => {
    res.render("Index", {pokemon: pokemon})
 })
+app.get('/pokemon/:id', (req, res) => {
+   res.send(req.params.id)
+})
+
+
+
+
 
 
 
